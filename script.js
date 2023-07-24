758546d89c2c59d4cd994c04da9b394e6d6491dc
Kezza01 <mitcheke41@craighead.school.nz>
2023-07-25 11:02:47 +1200

    Edited info
    age,species,name

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let nameArray = [
-'Lyla',
-'Ivy',
-"Flynn",
-'schnitzel von Krumm, with a very low tum',
+{name:'Lyla',age : 2, species :'rat'},
+{name:'Ivy',age : 1, species :'dog'},
+{name:'Flynn',age : 11, species :'dog'},
+{name:'schnitzel von Krumm, with a very low tum',age: 69, species:'hotdog'},
 ]
 
 function addName() {
