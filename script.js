6880e32b368f6a6e960fa5d85326882dd062aaaa
Kezza01 <mitcheke41@craighead.school.nz>
2023-08-03 10:10:45 +1200

    updated buttons

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-let nameArray = [
+let pets = [
 {name:'Lyla',age : 2, species :'rat'},
 {name:'Ivy',age : 1, species :'dog'},
 {name:'Flynn',age : 11, species :'dog'},
 {name:'schnitzel von Krumm, with a very low tum',age: 69, species:'hotdog'},
 ]
 
-function addName() {
-//lets user input the name of the pet
-let name=prompt("what is the name of your pet?")
-// add a new name to add to array of names
- nameArray.push(name)
+function showPets() {
+ for (let pet of pets) {
+    alert(pet.name)
+ }
 }
 
-function names(){
-//lets shows user names of all the pets
-alert(nameArray)
+
+
+function addPet() {
+//lets user input the name of the pet
+let name=prompt("what is the name of your pet?")
+let age= prompt("what is the age of " + name + "?")
+let species = prompt("What species is " + name + "?")
+// make an object called 'pet' from the details got
+let pet = {
+    name: name,
+    age: age,
+    species: species
+}
+//push the pet object onto the pets array
+pets.push(pet)
 }
